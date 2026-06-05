418806b6df803eb40badf3f29909f6aabe35fcf6
Hanna Wlodarska <hannawloda@gmail.com>
2026-06-05 14:03:54 +0200

    dodanie użytkowników i logowania

--- app/migrations/Version20260605110106.php
+++ app/migrations/Version20260605110106.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260605092641 extends AbstractMigration
+final class Version20260605110106 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -24,6 +24,7 @@ final class Version20260605092641 extends AbstractMigration
         $this->addSql('CREATE TABLE element (id INT AUTO_INCREMENT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, title VARCHAR(80) NOT NULL, author VARCHAR(80) DEFAULT NULL, year INT DEFAULT NULL, category_id INT NOT NULL, INDEX IDX_41405E3912469DE2 (category_id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE element_tag (element_id INT NOT NULL, tag_id INT NOT NULL, INDEX IDX_544124EB1F1F2A24 (element_id), INDEX IDX_544124EBBAD26311 (tag_id), PRIMARY KEY (element_id, tag_id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(64) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 (queue_name, available_at, delivered_at, id), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('ALTER TABLE element ADD CONSTRAINT FK_41405E3912469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
         $this->addSql('ALTER TABLE element_tag ADD CONSTRAINT FK_544124EB1F1F2A24 FOREIGN KEY (element_id) REFERENCES element (id) ON DELETE CASCADE');
@@ -40,6 +41,7 @@ final class Version20260605092641 extends AbstractMigration
         $this->addSql('DROP TABLE element');
         $this->addSql('DROP TABLE element_tag');
         $this->addSql('DROP TABLE tag');
+        $this->addSql('DROP TABLE users');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
