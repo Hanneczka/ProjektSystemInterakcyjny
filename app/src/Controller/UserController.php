218bdd3e6428c76b5d6c1978da5c2a15db0c782c
Hanna Wlodarska <hannawloda@gmail.com>
2026-06-06 13:45:22 +0200

    dodanie zmiany hasła i wywietlenia profilu użytkownika

---
+++ app/src/Controller/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\Type\PasswordType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class UserController extends AbstractController{
+
+    public function __construct(private readonly EntityManagerInterface $entityManager, private readonly TranslatorInterface $translator, private readonly UserPasswordHasherInterface $passwordHasher)
+    {
+
+    }
+
+    #[Route('/profile/edit-password', name: 'user_edit_password', methods: ['GET', 'POST'])]
+    public function editPassword(Request $request):Response{
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $form = $this->createForm(PasswordType::class, $user, [
+        'method'=>'POST',
+        'action'=> $this->generateUrl('user_edit_password')]
+    );
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $newPassword = $form->get('password')->getData();
+            $hashedPassword = $this->passwordHasher->hashPassword($user, $newPassword);
+            $user->setPassword($hashedPassword);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', $this->translator->trans('message.password_updated'));
+
+            return $this->redirectToRoute('user_profile', ['id' => $user->getId()]);
+
+        }
+        return $this->render('profile/edit_password.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route('/profile/{id}', name: 'user_profile', methods: ['GET'])]
+    public function view(UserRepository $userRepository, string $id): Response{
+        $user = $userRepository->findOneById($id);
+
+        if (null === $user) {
+            throw $this->createNotFoundException();
+        }
+
+
+        return $this->render(
+            'profile/profile.html.twig',
+            ['user' => $user]
+        );
+    }
+
+}
---
+++ app/src/Form/Type/PasswordType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType as SymfonyPasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PasswordType extends AbstractType{
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('password', RepeatedType::class, [
+            'type' => SymfonyPasswordType::class,
+            'first_options'  => [
+                'label' => 'label.new_password',
+                'required' => true,
+            ],
+            'second_options' => [
+                'label' => 'label.confirm_password',
+                'required' => true,
+            ],
+
+            'invalid_message' => 'form.invalid_password_error',
+        ]);
+    }
+    /**
+     * Configures the options for this type.
+     *
+     * @param OptionsResolver $resolver The resolver for the options
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(['data_class' => User::class]);
+    }
+
+    /**
+     * Returns the prefix of the template block name for this type.
+     *
+     * The block prefix defaults to the underscored short class name with
+     * the "Type" suffix removed (e.g. "UserProfileType" => "user_profile").
+     *
+     * @return string The prefix of the template block name
+     */
+    public function getBlockPrefix(): string
+    {
+        return 'user_password';
+    }
+}
